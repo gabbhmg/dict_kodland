c22878bf08c5e27ef03b0a8754278b23e6d32b09
gabbhmg <gnroma1203@gmail.com>
2024-11-29 16:24:50 +0100

    Delete password.py

--- password.py
+++
@@ -1,9 +0,0 @@
-import random 
-
-caratteri = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-lung_password = int(input("inserisci la lunghezza della password:"))
-password = ""
-for i in range(lung_password):
-    password += random.choice(caratteri)
-
-print("La tua password è:", password)
